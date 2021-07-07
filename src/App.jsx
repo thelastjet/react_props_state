fa52e54d6b1444aaa181f1f5b30a52ca3c662dac
Jay Thomas <thelastjet17@gmail.com>
2021-07-06 20:10:00 -0700

    made a few changes

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import "./App.css";
+import Box from "./components/Box";
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const boxes = [];
+    const numBoxes = 24;
+    for(let i =0; i < numBoxes; i++) {
+      boxes.push({
+      id: i,
+      color: `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`
+      });
+    }
+    
+
+    // set default state
+    this.state = {
+      boxes
+    };
+    // bind methods to this
+  }
+
+  handleBoxClick (e) {
+    const newBoxes = this.state.boxes.map((box) => {
+      if (box.id == e.target.id) {
+        box.color = `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`
+      }
+      return box;
+    })
+  }
+
+  getRandomColor() {
+    const rgb = Math.round(Math.random() * 255);
+    return rgb;
+  }
+
+  render() {
+    const renderBoxes = this.state.boxes.map((box) => (
+      <Box 
+        key = {box.id}
+        id = {box.id}
+        color = {box.color}
+        handleClick = {this.handleBoxClick}  
+      />
+    ));
+    return (
+      <main
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          textAlign: "center",
+        }}
+      >
+        <h1>React: State and Props</h1>
+        <div className="App">{renderBoxes}</div>
+      </main>
+    );
+  }
+}
+
+export default App;
